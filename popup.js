bcd20fb0cf6da2f9527bfc3a5f3e8a6bb671665a
Pratham Shroff <pshroff@bu.edu>
2021-11-13 21:36:08 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,26 @@
 // Future JavaScript will go here
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').onsubmit = function() {
+        const appt = document.querySelector('#appt').value;    // e.g. s = "21:00"
+        const reminder = document.querySelector('#reminder').value;
+
+        hour = parseInt(appt.slice(0, 2)); // 21
+        min = parseInt(appt.slice(3, 5));
+        if (hour >= 12) {
+            meridian = "PM";
+            hour = hour - 12;
+        }
+        else {
+            meridian = "AM";
+        }
+
+        if (hour === 0) {
+            hour = 12;
+        }
+
+        const r = new Reminder(hour, min, meridian, reminder);
+
+        alert(`Your reminder is: ${reminder} at ${hour} & ${min} & ${meridian}`);
+    };
+});
